027ae95462f112693eb88c10dc6882d497e39d7c
franciscosucho <110209358+franciscosucho@users.noreply.github.com>
2023-10-20 10:39:24 -0300

    20/10/23_2

--- bd.sql
+++ bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-10-2023 a las 18:53:06
+-- Tiempo de generación: 20-10-2023 a las 15:38:49
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -22,33 +22,33 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `clinica` (
-  `id_clinica` int(11) NOT NULL AUTO_INCREMENT,
+  `id_clinica` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `direccion` varchar(30) NOT NULL,
   `telefono` varchar(30) NOT NULL,
-  PRIMARY KEY (`id_clinica`)
+  `horario_atencion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `clinica`
 --
 
-INSERT INTO `clinica` (`id_clinica`, `nombre`, `direccion`, `telefono`) VALUES
-(1, 'Clinica A', 'Calle 123', '123-456-7890'),
-(2, 'Clinica B', 'Avenida 456', '987-654-3210'),
-(3, 'Clinica C', 'Calle 789', '555-123-4567'),
-(4, 'Clinica D', 'Avenida 1011', '111-222-3333'),
-(5, 'Clinica E', 'Calle 1213', '444-555-6666'),
-(6, 'Clinica F', 'Avenida 1415', '777-888-9999'),
-(7, 'Clinica G', 'Calle 1617', '333-444-5555'),
-(8, 'Clinica H', 'Avenida 1819', '666-777-8888'),
-(9, 'Clinica I', 'Calle 2021', '999-111-2222'),
-(10, 'Clinica J', 'Avenida 2223', '222-333-4444'),
-(11, 'Clinica K', 'Calle 2425', '888-999-0000'),
-(12, 'Clinica L', 'Avenida 2627', '444-555-6666'),
-(13, 'Clinica M', 'Calle 2829', '111-222-3333'),
-(14, 'Clinica N', 'Avenida 3031', '555-666-7777'),
-(15, 'Clinica O', 'Calle 3233', '999-888-7777');
+INSERT INTO `clinica` (`id_clinica`, `nombre`, `direccion`, `telefono`, `horario_atencion`) VALUES
+(1, 'Clinica A', 'Calle 123', '123-456-7890', 'De 7AM A 6PM'),
+(2, 'Clinica B', 'Avenida 456', '987-654-3210', 'De 8AM A 7PM'),
+(3, 'Clinica C', 'Calle 789', '555-123-4567', 'De 7AM A 6PM'),
+(4, 'Clinica D', 'Avenida 1011', '111-222-3333', 'De 5AM A 6PM'),
+(5, 'Clinica E', 'Calle 1213', '444-555-6666', 'De 6AM A 6PM'),
+(6, 'Clinica F', 'Avenida 1415', '777-888-9999', 'De 6.30AM A 6PM'),
+(7, 'Clinica G', 'Calle 1617', '333-444-5555', 'De 7AM A 7PM'),
+(8, 'Clinica H', 'Avenida 1819', '666-777-8888', 'De 7AM A 6PM'),
+(9, 'Clinica I', 'Calle 2021', '999-111-2222', 'De 7AM A 6PM'),
+(10, 'Clinica J', 'Avenida 2223', '222-333-4444', 'De 7AM A 6PM'),
+(11, 'Clinica K', 'Calle 2425', '888-999-0000', 'De 7AM A 4PM'),
+(12, 'Clinica L', 'Avenida 2627', '444-555-6666', 'De 7AM A 6PM'),
+(13, 'Clinica M', 'Calle 2829', '111-222-3333', 'De 7AM A 6PM'),
+(14, 'Clinica N', 'Avenida 3031', '555-666-7777', 'De 7AM A 6PM'),
+(15, 'Clinica O', 'Calle 3233', '999-888-7777', 'De 7AM A 6PM');
 
 -- --------------------------------------------------------
 
@@ -57,15 +57,12 @@ INSERT INTO `clinica` (`id_clinica`, `nombre`, `direccion`, `telefono`) VALUES
 --
 
 CREATE TABLE `empleado` (
-  `id_empleado` int(11) NOT NULL AUTO_INCREMENT,
+  `id_empleado` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(30) NOT NULL,
   `contacto` varchar(30) NOT NULL,
   `cargo` varchar(30) NOT NULL,
-  `clinica` int(11) NOT NULL,
-  PRIMARY KEY (`id_empleado`),
-  UNIQUE KEY `clinica` (`clinica`),
-  FOREIGN KEY (`clinica`) REFERENCES `clinica` (`id_clinica`)
+  `clinica` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -96,14 +93,11 @@ INSERT INTO `empleado` (`id_empleado`, `nombre`, `apellido`, `contacto`, `cargo`
 --
 
 CREATE TABLE `historial_vacunas` (
-  `id_vacuna` int(11) NOT NULL AUTO_INCREMENT,
+  `id_vacuna` int(11) NOT NULL,
   `nombre_vacuna` varchar(30) NOT NULL,
   `fecha_aplicacion` date NOT NULL,
   `observaciones` varchar(90) NOT NULL,
-  `cod_historia` int(11) NOT NULL,
-  PRIMARY KEY (`id_vacuna`),
-  UNIQUE KEY `cod_historia` (`cod_historia`),
-  FOREIGN KEY (`cod_historia`) REFERENCES `historia_clinica` (`cod_historial`)
+  `cod_historia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -124,15 +118,12 @@ INSERT INTO `historial_vacunas` (`id_vacuna`, `nombre_vacuna`, `fecha_aplicacion
 --
 
 CREATE TABLE `historia_clinica` (
-  `cod_historial` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_historial` int(11) NOT NULL,
   `enfermedades` varchar(30) NOT NULL,
   `alergias` varchar(30) NOT NULL,
   `tipo_sangre` varchar(3) NOT NULL,
   `ultima_visita` date NOT NULL,
-  `paciente` int(11) NOT NULL,
-  PRIMARY KEY (`cod_historial`),
-  UNIQUE KEY `paciente` (`paciente`),
-  FOREIGN KEY (`paciente`) REFERENCES `pacientes` (`dni`)
+  `paciente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -159,32 +150,48 @@ INSERT INTO `historia_clinica` (`cod_historial`, `enfermedades`, `alergias`, `ti
 --
 
 CREATE TABLE `inventario` (
-  `id_producto` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id_producto` int(11) NOT NULL,
   `nombre` varchar(33) NOT NULL,
   `fabricante` varchar(33) NOT NULL,
   `stock` int(11) NOT NULL,
   `efectividad_en_dias` int(11) NOT NULL,
-  `dirigido_para` varchar(40) NOT NULL
+  `dirigido_para` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `inventario`
 --
 
-INSERT INTO `inventario` (`nombre`, `fabricante`, `stock`, `efectividad_en_dias`, `dirigido_para`) VALUES
-('Hepatitis B', 'Bayern', 200, 20, 'Recién nacidos'),
-('BCG (Tuberculosis)', 'Galeno', 30, 10, 'Recién nacidos'),
-('Polio IPV (Salk)', '', 200, 10, 'Lactantes'),
-('Quíntuple / Pentavalente', '', 200, 10, 'Lactantes'),
-('Neumococo Conjugada (Neumo 13)', '', 200, 10, 'Lactantes'),
-('Rotavirus', '', 100, 20, 'Lactantes'),
-('Meningococo', '', 200, 40, '3 Meses'),
-('Polio IPV (Salk)', '', 40, 30, '4 Meses'),
-('Quíntuple / Pentavalente', '', 40, 300, '4 Meses'),
-('Neumococo Conjugada (Neumo 13)', '', 20, 40, '4 Meses'),
-('Rotavirus', '', 200, 100, '4 Meses'),
-('Doble Bacteriana (cada 10 años)', '', 0, 0, 'ADULTOS'),
-('Hepatitis B', '', 0, 0, 'ADULTOS');
+INSERT INTO `inventario` (`id_producto`, `nombre`, `fabricante`, `stock`, `efectividad_en_dias`, `dirigido_para`) VALUES
+(1, 'Producto A', 'Fabricante 1', 344, 24, ''),
+(2, 'Producto B', 'Fabricante 2', 75, 270, ''),
+(3, 'Producto C', 'Fabricante 3', 50, 180, ''),
+(4, 'Producto D', 'Fabricante 4', 125, 300, ''),
+(5, 'Producto E', 'Fabricante 5', 90, 240, ''),
+(6, 'Producto F', 'Fabricante 6', 60, 120, ''),
+(7, 'Producto G', 'Fabricante 7', 110, 210, ''),
+(8, 'Producto H', 'Fabricante 8', 70, 150, ''),
+(9, 'Producto I', 'Fabricante 9', 85, 180, ''),
+(10, 'Producto J', 'Fabricante 10', 120, 365, ''),
+(11, 'Producto K', 'Fabricante 11', 55, 90, ''),
+(12, 'Producto L', 'Fabricante 12', 95, 210, ''),
+(13, 'Producto M', 'Fabricante 13', 70, 120, ''),
+(14, 'Producto N', 'Fabricante 14', 150, 365, ''),
+(15, 'Producto O', 'Fabricante 15', 80, 240, ''),
+(16, 'Hepatitis B', 'Bayern', 200, 20, 'Recién nacidos'),
+(17, 'BCG (Tuberculosis)', 'Galeno', 30, 10, 'Recién nacidos'),
+(18, 'Polio IPV (Salk)', '', 200, 10, 'Lactantes'),
+(19, 'Quíntuple / Pentavalente', '', 200, 10, 'Lactantes'),
+(20, 'Neumococo Conjugada (Neumo 13)', '', 200, 10, 'Lactantes'),
+(21, 'Rotavirus', '', 100, 20, 'Lactantes'),
+(22, 'Meningococo', '', 200, 40, '3 Meses'),
+(23, 'Polio IPV (Salk)', '', 40, 30, '4 Meses'),
+(24, 'Quíntuple / Pentavalente', '', 40, 300, '4 Meses'),
+(25, 'Neumococo Conjugada (Neumo 13)', '', 20, 40, '4 Meses'),
+(26, 'Rotavirus', '', 200, 100, '4 Meses'),
+(27, 'Doble Bacteriana (cada 10 años)', '', 0, 0, 'ADULTOS'),
+(28, 'Hepatitis B', '', 0, 0, 'ADULTOS'),
+(29, 'eumococo Conjugada (Neumo 11)', 'bayern', 100, 10, '3 Meses');
 
 -- --------------------------------------------------------
 
@@ -196,9 +203,7 @@ CREATE TABLE `jornada_laboral` (
   `dia` date NOT NULL,
   `horario_entrada` time NOT NULL,
   `horario_salida` time NOT NULL,
-  `empleado` int(11) NOT NULL,
-  PRIMARY KEY (`dia`,`empleado`),
-  FOREIGN KEY (`empleado`) REFERENCES `empleado` (`id_empleado`)
+  `empleado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -206,16 +211,21 @@ CREATE TABLE `jornada_laboral` (
 --
 
 INSERT INTO `jornada_laboral` (`dia`, `horario_entrada`, `horario_salida`, `empleado`) VALUES
-('2023-09-25', '08:00:00', '17:00:00', 1),
-('2023-09-26', '08:00:00', '17:00:00', 1),
-('2023-09-27', '08:00:00', '17:00:00', 1),
-('2023-09-28', '08:00:00', '17:00:00', 1),
-('2023-09-29', '08:00:00', '17:00:00', 1),
-('2023-09-25', '07:00:00', '16:00:00', 2),
-('2023-09-26', '07:00:00', '16:00:00', 2),
-('2023-09-27', '07:00:00', '16:00:00', 2),
-('2023-09-28', '07:00:00', '16:00:00', 2),
-('2023-09-29', '07:00:00', '16:00:00', 2);
+('2023-09-08', '08:00:00', '16:00:00', 1),
+('2023-09-09', '09:00:00', '17:00:00', 2),
+('2023-09-10', '07:30:00', '15:30:00', 3),
+('2023-09-11', '08:30:00', '16:30:00', 4),
+('2023-09-12', '07:00:00', '15:00:00', 5),
+('2023-09-13', '09:30:00', '17:30:00', 6),
+('2023-09-14', '08:15:00', '16:15:00', 7),
+('2023-09-15', '09:15:00', '17:15:00', 8),
+('2023-09-16', '08:45:00', '16:45:00', 9),
+('2023-09-17', '07:45:00', '15:45:00', 10),
+('2023-09-18', '09:45:00', '17:45:00', 11),
+('2023-09-19', '08:20:00', '16:20:00', 12),
+('2023-09-20', '09:20:00', '17:20:00', 13),
+('2023-09-21', '07:20:00', '15:20:00', 14),
+('2023-09-22', '08:50:00', '16:50:00', 15);
 
 -- --------------------------------------------------------
 
@@ -224,29 +234,236 @@ INSERT INTO `jornada_laboral` (`dia`, `horario_entrada`, `horario_salida`, `empl
 --
 
 CREATE TABLE `pacientes` (
-  `dni` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(30) NOT NULL,
-  `telefono` varchar(30) NOT NULL,
-  `direccion` varchar(30) NOT NULL,
-  `nacimiento` date NOT NULL,
-  PRIMARY KEY (`dni`)
+  `contacto` varchar(30) NOT NULL,
+  `fecha_nac` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `pacientes`
 --
 
-INSERT INTO `pacientes` (`dni`, `nombre`, `apellido`, `telefono`, `direccion`, `nacimiento`) VALUES
-(4634635, 'Paciente A', 'Apellido A', '123-456-7890', 'Dirección A', '2000-01-01'),
-(7891234, 'Paciente B', 'Apellido B', '987-654-3210', 'Dirección B', '1985-03-15'),
-(2345678, 'Paciente C', 'Apellido C', '555-123-4567', 'Dirección C', '1999-07-20'),
-(1234567, 'Paciente D', 'Apellido D', '111-222-3333', 'Dirección D', '1972-11-30'),
-(9876543, 'Paciente E', 'Apellido E', '444-555-6666', 'Dirección E', '2005-04-05'),
-(3456789, 'Paciente F', 'Apellido F', '777-888-9999', 'Dirección F', '1993-12-10'),
-(5678901, 'Paciente G', 'Apellido G', '333-444-5555', 'Dirección G', '1982-06-25'),
-(8901234, 'Paciente H', 'Apellido H', '666-777-8888', 'Dirección H', '1968-09-18'),
-(6789012, 'Paciente I', 'Apellido I', '999-111-2222', 'Dirección I', '1990-07-31'),
-(4567890, 'Paciente J', 'Apellido J', '222-333-4444', 'Dirección J', '1977-08-09'),
-(1237890, 'Paciente K', 'Apellido K', '888-999-0000', 'Dirección K', '1994-11-05');
+INSERT INTO `pacientes` (`dni`, `nombre`, `apellido`, `contacto`, `fecha_nac`) VALUES
+(1234567, 'Ana', 'Rodríguez', 'ana@email.com', '1988-05-03'),
+(1237890, 'Mateo', 'López', 'mateo@email.com', '1996-02-19'),
+(2345678, 'Pedro', 'García', 'pedro@email.com', '1992-07-10'),
+(3456789, 'Laura', 'Sánchez', 'laura@email.com', '1984-09-17'),
+(4567890, 'Valentina', 'Pérez', 'valentina@email.com', '1994-08-30'),
+(4634635, 'Maria', 'Polardo', 'polardo@gmail.com', '1999-06-02'),
+(5678901, 'Carlos', 'Ramírez', 'carlos@email.com', '1997-04-08'),
+(6789012, 'Diego', 'González', 'diego@email.com', '1993-06-14'),
+(7891234, 'María', 'López', 'maria@email.com', '1985-03-22'),
+(8901234, 'Sofía', 'Torres', 'sofia@email.com', '1991-12-01'),
+(9876543, 'Luis', 'Martínez', 'luis@email.com', '1995-11-28');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `id_registro` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `horario` time NOT NULL,
+  `empleado` int(11) NOT NULL,
+  `id_vacuna` int(11) NOT NULL,
+  `paciente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `registro`
+--
+
+INSERT INTO `registro` (`id_registro`, `fecha`, `horario`, `empleado`, `id_vacuna`, `paciente`) VALUES
+(1, '2023-09-14', '26:36:59', 5, 1, 4634635),
+(2, '2023-09-08', '08:00:00', 1, 5, 1237890),
+(3, '2023-09-09', '09:15:00', 2, 2, 2345678),
+(4, '2023-09-10', '10:30:00', 3, 3, 3456789),
+(5, '2023-09-11', '11:45:00', 4, 4, 4567890);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitud_turno`
+--
+
+CREATE TABLE `solicitud_turno` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `localidad` varchar(30) NOT NULL,
+  `descripcion` varchar(120) NOT NULL,
+  `telefono` int(20) NOT NULL,
+  `correo` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `solicitud_turno`
+--
+
+INSERT INTO `solicitud_turno` (`id`, `nombre`, `localidad`, `descripcion`, `telefono`, `correo`) VALUES
+(1, 'jose', 'Olivos', 'vacunarme contra el covid', 12141241, 'dante@gmail.com'),
+(2, 'jose', 'Olivos', 'vacunarme contra el covid', 12141241, 'dante@gmail.com'),
+(3, '', 'Boulogne', '', 0, ''),
+(4, '', 'Boulogne', '', 0, ''),
+(5, '', 'Boulogne', '', 0, ''),
+(6, '', 'Boulogne', '', 0, ''),
+(7, '', 'Boulogne', '', 0, ''),
+(8, '', 'Boulogne', '', 0, ''),
+(9, '', 'Boulogne', '', 0, ''),
+(10, '', 'Boulogne', '', 0, ''),
+(11, '', 'Boulogne', '', 0, ''),
+(12, '', 'Boulogne', '', 0, ''),
+(13, '', 'Boulogne', '', 0, ''),
+(14, 'maria', 'vicentelopez', 'vacunarme contra el covid', 12141241, 'dante@gmail.com'),
+(15, 'fran', 'Boulogne', 'vacunarme contra el covid', 12141241, 'anabarrio@gmal.com'),
+(16, 'milo j', 'Villa Adelina', 'vacunarme para la gripe', 124142121, 'miloj.@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clinica`
+--
+ALTER TABLE `clinica`
+  ADD PRIMARY KEY (`id_clinica`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD UNIQUE KEY `clinica` (`clinica`);
+
+--
+-- Indices de la tabla `historial_vacunas`
+--
+ALTER TABLE `historial_vacunas`
+  ADD PRIMARY KEY (`id_vacuna`),
+  ADD UNIQUE KEY `cod_historia` (`cod_historia`);
+
+--
+-- Indices de la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  ADD PRIMARY KEY (`cod_historial`),
+  ADD UNIQUE KEY `paciente` (`paciente`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
+-- Indices de la tabla `jornada_laboral`
+--
+ALTER TABLE `jornada_laboral`
+  ADD UNIQUE KEY `empleado` (`empleado`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id_registro`),
+  ADD UNIQUE KEY `paciente` (`paciente`),
+  ADD KEY `empleado` (`empleado`),
+  ADD KEY `id_vacuna` (`id_vacuna`);
+
+--
+-- Indices de la tabla `solicitud_turno`
+--
+ALTER TABLE `solicitud_turno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clinica`
+--
+ALTER TABLE `clinica`
+  MODIFY `id_clinica` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `historial_vacunas`
+--
+ALTER TABLE `historial_vacunas`
+  MODIFY `id_vacuna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  MODIFY `cod_historial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `dni` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9876544;
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud_turno`
+--
+ALTER TABLE `solicitud_turno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`clinica`) REFERENCES `clinica` (`id_clinica`);
+
+--
+-- Filtros para la tabla `historial_vacunas`
+--
+ALTER TABLE `historial_vacunas`
+  ADD CONSTRAINT `historial_vacunas_ibfk_1` FOREIGN KEY (`cod_historia`) REFERENCES `historia_clinica` (`cod_historial`);
+
+--
+-- Filtros para la tabla `historia_clinica`
+--
+ALTER TABLE `historia_clinica`
+  ADD CONSTRAINT `historia_clinica_ibfk_1` FOREIGN KEY (`paciente`) REFERENCES `pacientes` (`dni`);
+
+--
+-- Filtros para la tabla `jornada_laboral`
+--
+ALTER TABLE `jornada_laboral`
+  ADD CONSTRAINT `jornada_laboral_ibfk_1` FOREIGN KEY (`empleado`) REFERENCES `empleado` (`id_empleado`);
+
+--
+-- Filtros para la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`paciente`) REFERENCES `pacientes` (`dni`),
+  ADD CONSTRAINT `registro_ibfk_2` FOREIGN KEY (`empleado`) REFERENCES `empleado` (`id_empleado`),
+  ADD CONSTRAINT `registro_ibfk_3` FOREIGN KEY (`id_vacuna`) REFERENCES `inventario` (`id_producto`);
 COMMIT;
